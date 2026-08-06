72c16a67a0c026f5fb57f489fbc005c83c17b709
srilathasagili-crypto <srilathasagili@gmail.com>
2026-08-06 15:26:55 +0530

    Update import statement for PlacesService

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import pandas as pd
 import pydeck as pdk
 import streamlit as st
 from dotenv import load_dotenv
-from services.places import PlacesService
 from services.places_service import PlacesService
 
 load_dotenv()
